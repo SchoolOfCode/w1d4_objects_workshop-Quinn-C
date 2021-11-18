80d5f78c6eb2af562a3e10f2388d134a5d4be7bf
Quinn.C <joanchenuk@gmail.com>
2021-11-18 12:18:44 +0000

    task1 finished

---
+++ main.js
@@ -0,0 +1,25 @@
+//Task1
+let house = {
+    stores: 2,
+    parking: false,
+    number_of_bedrooms: 2,
+    number_of_garden: 1,
+    energy_efficiency_rating: "D",
+
+};
+
+let car = {
+    color: "red",
+    number_of_wheel: 4,
+    steering: power,
+    number_of_seats: 2,
+    consumption: '20 miles/gallon',
+
+};
+
+
+let favorite_movie = {
+    title: `The Dark Knight`,
+    released_year: `2008`,
+    lead_actor: `Christian Bale`,
+};
