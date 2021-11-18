7ffa2f877241454e8a373484e952ccbde9bc0582
Quinn.C <joanchenuk@gmail.com>
2021-11-18 20:00:28 +0000

    task3 finished

---
+++ main3.js
@@ -0,0 +1,11 @@
+//Task3
+let desiredPleasantry = "greeting";
+
+desiredPleasantry = "farewell";
+let bensPhrases = {
+    greeting: "Well hello there!",
+    farewell: "See you in another life brother",
+    smallTalk: "Warm today isn't it...",
+};
+
+console.log(`${bensPhrases[desiredPleasantry]}`);
